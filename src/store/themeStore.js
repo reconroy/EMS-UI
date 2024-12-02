1273918f04c60f8f8ba92be6612e8540a24cc0f6
reconroy <codewithroy22@gmail.com>
2024-12-02 23:15:59 +0530

    updated default theme dark

--- src/store/themeStore.js
+++ src/store/themeStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 
 const useThemeStore = create((set) => ({
-  theme: localStorage.getItem('theme') || 'dark',
+  theme: localStorage.getItem('theme') || 'light',
   toggleTheme: () => set((state) => {
     const newTheme = state.theme === 'dark' ? 'light' : 'dark';
     localStorage.setItem('theme', newTheme);
